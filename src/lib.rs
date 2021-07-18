3fe8255f5bde1bd33db27a5fdf1e26312c964b38
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-18 17:13:08 +1000

    Add new struct signal_mask::SignalMask and impl associated func for it
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,13 @@ extern crate num_enum;
 pub use libc::{pid_t, siginfo_t};
 
 mod signal;
+mod signal_mask;
 pub mod utility;
 mod fd;
 mod signal_fd;
 mod pid_fd;
 
 pub use signal::Signal;
-
+pub use signal_mask::SignalMask;
 pub use signal_fd::{SignalFd, ArrayVec};
 pub use pid_fd::*;
---
+++ src/signal_mask.rs
@@ -0,0 +1,58 @@
+use std::io::{Result, Error};
+use libc::{sigset_t, SIG_BLOCK, sigemptyset, sigaddset, sigprocmask};
+
+use crate::Signal;
+
+#[derive(Copy, Clone)]
+pub struct SignalMask {
+    mask: sigset_t
+}
+impl Default for SignalMask {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+impl SignalMask {
+    /// Create an empty `SignalMask`.
+    ///
+    /// This is the same as `Default::default()` for `SignalMask`.
+    pub fn new() -> Self {
+        let mut mask = std::mem::MaybeUninit::<sigset_t>::uninit();
+
+        let ret = unsafe { sigemptyset(mask.as_mut_ptr()) };
+        if cfg!(debug_assertions) && ret < 0 {
+            let result: Result<()> = Err(Error::last_os_error());
+            result.unwrap();
+        }
+
+        Self { mask: unsafe { mask.assume_init() } }
+    }
+
+    /// Add `signal` to the mask.
+    pub fn add(&mut self, signal: Signal) -> Result<()> {
+        if unsafe { sigaddset(&mut self.mask, signal.into()) } < 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Block the signal specified in mask and return the old signal mask.
+    pub fn block(&self) -> Result<SignalMask> {
+        let mut old_mask = std::mem::MaybeUninit::<sigset_t>::uninit();
+
+        if unsafe { sigprocmask(SIG_BLOCK, &self.mask, old_mask.as_mut_ptr()) } < 0 {
+            Err(Error::last_os_error())
+        } else {
+            Ok(Self { mask: unsafe { old_mask.assume_init() } })
+        }
+    }
+
+    pub fn as_sigset(&self) -> &sigset_t {
+        &self.mask
+    }
+
+    pub fn as_sigset_mut(&mut self) -> &mut sigset_t {
+        &mut self.mask
+    }
+}
