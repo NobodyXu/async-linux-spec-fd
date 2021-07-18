42b37e718afbed286b3d1df48c8e391e70c23ea2
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-18 17:31:15 +1000

    Add missing doc for fn and macro in utility.rs
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/utility.rs
+++ src/utility.rs
@@ -1,5 +1,6 @@
 use std::io::{Result, ErrorKind};
 
+/// auto restart functions failed due to interrpted
 pub fn autorestart<T, F>(mut f: F)
     -> Result<T>
     where F: FnMut() -> Result<T>
@@ -16,6 +17,8 @@ pub fn autorestart<T, F>(mut f: F)
         break ret
     }
 }
+
+/// auto restart functions failed due to interrpted
 #[macro_export]
 macro_rules! autorestart {
     ( { $( $tt:tt )* } ) => {
