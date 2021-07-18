aa8cf4ec3cef70aea6135d2a046bf7cca3c7e473
Jiahao XU <Jiahao_XU@outlook.com>
2021-07-18 16:49:35 +1000

    Refactor signal_fd::Signal into its own module
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,15 @@ extern crate num_enum;
 
 pub use libc::{pid_t, siginfo_t};
 
+mod signal;
 pub mod utility;
 mod fd;
 mod signal_fd;
 mod children_reaper;
 mod pid_fd;
 
-pub use signal_fd::{Signal, SignalFd, ArrayVec};
+pub use signal::Signal;
+
+pub use signal_fd::{SignalFd, ArrayVec};
 pub use children_reaper::{Reaper, ExitInfo, ChildTermSignal};
 pub use pid_fd::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/signal_fd.rs
+++ src/signal_fd.rs
@@ -11,36 +11,8 @@ use tokio::io::Interest;
 
 pub use arrayvec::ArrayVec;
 
-use num_enum::{IntoPrimitive, TryFromPrimitive};
-
 use crate::fd::Fd;
-
-// Here it relies on the compiler to check that i32 == c_int
-#[repr(i32)]
-#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, IntoPrimitive, TryFromPrimitive)]
-pub enum Signal {
-    Sigabrt   = libc::SIGABRT,
-    Sigchld  = libc::SIGCHLD,
-    Sigcont  = libc::SIGCONT,
-    Sighup   = libc::SIGHUP,
-    Sigint   = libc::SIGINT,
-    Sigio    = libc::SIGIO,
-    Sigkill   = libc::SIGKILL,
-    Sigprof   = libc::SIGPROF,
-    Sigpipe  = libc::SIGPIPE,
-    Sigpwr   = libc::SIGPWR,
-    Sigquit  = libc::SIGQUIT,
-    Sigtstp  = libc::SIGTSTP,
-    Sigttin  = libc::SIGTTIN,
-    Sigttou  = libc::SIGTTOU,
-    Sigurg   = libc::SIGURG,
-    Sigusr1  = libc::SIGUSR1,
-    Sigusr2  = libc::SIGUSR2,
-    Sigvtalrm = libc::SIGVTALRM,
-    Sigwinch = libc::SIGWINCH,
-    Sigxcpu  = libc::SIGXCPU,
-    Sigxfsz  = libc::SIGXFSZ,
-}
+use crate::Signal;
 
 /// Due to the fact that epoll on signalfd would fail after fork, you cannot reuse
 /// SignalFd after forked.
